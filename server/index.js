3b73236afe92a25e33016a991641eae4de71a6e6
dumbcheese <44883387+dumbcheese@users.noreply.github.com>
2021-02-07 15:47:02 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 
-// const CONNECTION_URL = 'mongodb+srv://damirh:SevenSamurai7@cluster0.dypsy.mongodb.net/<dbname>?retryWrites=true&w=majority';
+// const CONNECTION_URL = 'connection URI for Atlas'
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
